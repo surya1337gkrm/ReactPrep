1ba12d7c43216fac71e5ed2b7b9722c1e4c86492
surya1337gkrm <suryavenkatesh0@gmail.com>
2023-03-12 17:53:28 -0400

    cart page

--- FoodApp/src/components/cart.js
+++ FoodApp/src/components/cart.js
@@ -1,14 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { emptyCartURl } from '../config';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { clearCart } from '../utils/cartSlice';
 
 function Cart() {
   // const [cart, setCart] = useState(null);
   const cart = useSelector((store) => store.cart.items);
+  const [cartTotal, setCartTotal] = useState(0);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    let total = 0;
+    cart.forEach((item) => {
+      total += item.price;
+    });
+    setCartTotal(total / 100);
+  });
 
   return cart.length !== 0 ? (
-    <div className='flex justify-center items-center '>cart</div>
+    <div className='flex justify-center items-center '>
+      <div className='flex-col'>
+        <h1 className='text-2xl font-bold py-2 my-2'>Cart</h1>
+        {cart.map((item) => (
+          <div key={item.id} className='bg-slate-100 shadow p-2 my-2'>
+            {item.name}
+          </div>
+        ))}
+        Cart Total:
+        <small className="font-bold before:content-['\20B9']">
+          {cartTotal}
+        </small>
+        <br />
+        <button
+          className='bg-red-400 p-2 my-2 rounded text-white font-bold'
+          onClick={() => dispatch(clearCart())}>
+          Clear Cart
+        </button>
+      </div>
+    </div>
   ) : (
     <>
       <div className='flex justify-center items-center '>
