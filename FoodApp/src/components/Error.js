13affa3932d49b2623f9fc1255b91d4f655eca10
surya1337gkrm <suryavenkatesh0@gmail.com>
2023-03-22 19:20:38 -0400

    Error Component

--- FoodApp/src/components/Error.js
+++ FoodApp/src/components/Error.js
@@ -12,21 +12,21 @@ const Error = () => {
         <small>{`${err.status}: ${err.statusText}`}</small>
       </div> */}
       <div className='h-full w-full m-5 p-5'>
-        <main class='h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]'>
-          <h1 class='text-9xl font-extrabold text-white tracking-widest'>
+        <main className='h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]'>
+          <h1 className='text-9xl font-extrabold text-white tracking-widest'>
             {err.status}
           </h1>
-          <div class='bg-[#FF6A3D] px-5 m-5 text-sm rounded -rotate-12 absolute'>
+          <div className='bg-[#FF6A3D] px-5 m-5 text-sm rounded -rotate-12 absolute'>
             {err.statusText}
           </div>
-          <button class='mt-5'>
-            <a class='relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring'>
-              <span class='absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0'></span>
+          <button className='mt-5'>
+            <div className='relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring'>
+              <span className='absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0'></span>
 
-              <span class='relative block px-8 py-3 bg-[#1A2238] border border-current'>
+              <span className='relative block px-8 py-3 bg-[#1A2238] border border-current'>
                 <Link to='/'>Go Home</Link>
               </span>
-            </a>
+            </div>
           </button>
         </main>
       </div>
